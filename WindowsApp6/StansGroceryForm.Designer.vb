f3a1affa61b423c4a7fe866e623dc74ba8186c7a
rasset69@gmail.com <rasset69@gmail.com>
2022-04-25 08:55:58 -0600

    Add project files.

---
+++ WindowsApp6/StansGroceryForm.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class StansGroceryForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DisplayListBox = New System.Windows.Forms.ListBox()
+        Me.SearchButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'DisplayListBox
+        '
+        Me.DisplayListBox.FormattingEnabled = True
+        Me.DisplayListBox.ItemHeight = 16
+        Me.DisplayListBox.Location = New System.Drawing.Point(288, 37)
+        Me.DisplayListBox.Name = "DisplayListBox"
+        Me.DisplayListBox.Size = New System.Drawing.Size(395, 244)
+        Me.DisplayListBox.TabIndex = 0
+        '
+        'SearchButton
+        '
+        Me.SearchButton.Location = New System.Drawing.Point(144, 333)
+        Me.SearchButton.Name = "SearchButton"
+        Me.SearchButton.Size = New System.Drawing.Size(75, 23)
+        Me.SearchButton.TabIndex = 1
+        Me.SearchButton.Text = "Search"
+        Me.SearchButton.UseVisualStyleBackColor = True
+        '
+        'StansGroceryForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.SearchButton)
+        Me.Controls.Add(Me.DisplayListBox)
+        Me.Name = "StansGroceryForm"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents DisplayListBox As ListBox
+    Friend WithEvents SearchButton As Button
+End Class
---
+++ WindowsApp6/StansGroceryForm.vb
@@ -0,0 +1,56 @@
+﻿Option Explicit On
+Option Strict On
+Public Class StansGroceryForm
+    Dim food(500, 2) As String
+
+    Sub LoadDataFile()
+        Dim filename As String = "C:\Users\Seth\Desktop\Visual Basic\grocery.txt" ' set up own path on machine 
+        Dim record As String
+        Dim row As Integer
+        Dim temp() As String
+        FileOpen(1, filename, OpenMode.Input)
+
+
+        Do Until EOF(1)
+
+            'grab item name 
+            Input(1, record)
+            temp = Split(record, "$$ITM")
+            food(row, 0) = temp(1)
+
+            'grab location
+            Input(1, record)
+            food(row, 1) = record
+
+            'grab category  
+            Input(1, record)
+            food(row, 2) = record
+
+            'DisplayListBox.Items.Add(record)
+            row += 1
+        Loop
+
+        FileClose(1)
+
+    End Sub
+
+    Sub ListBoxDisplay()
+
+        For i = Me.food.GetLowerBound(0) To Me.food.GetUpperBound(0)
+            DisplayListBox.Items.Add($"{Me.food(i, 0)} : {Me.food(i, 1)} : {Me.food(i, 2)}")
+        Next
+
+    End Sub
+
+    'Look for file load it
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        LoadDataFile()
+        ListBoxDisplay()
+
+    End Sub
+
+    Private Sub SearchButton_Click(sender As Object, e As EventArgs) Handles SearchButton.Click
+        LoadDataFile()
+
+    End Sub
+End Class
